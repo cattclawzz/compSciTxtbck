eea7b9f6b05c7f620b8913752305e0c3fa1bc9ce
cattclawzz <anakinedwards9@gmail.com>
2025-07-18 06:26:38 +0100

    changed the age to stay up to date

--- ch1_IntroToPython/pg18/q1.py
+++ ch1_IntroToPython/pg18/q1.py
@@ -1,6 +1,10 @@
 '''
-create a variable to store teh value of your age. Assign the value of your age to the variable and display it with the following text: "What a great age!"
+create a variable to store the value of your age. Assign the value of your age to the variable and display it with the following text: "What a great age!"
 '''
+import datetime
 
-age = 16
+def getAge(DOB):
+    return (datetime.datetime.now() - DOB).days // 365
+
+age = getAge(datetime.datetime(2009, 3, 7))
 print(f"{age}, What a great age!")
